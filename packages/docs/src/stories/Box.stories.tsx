2f742cc88cf348b7d8a389fbbfb9c11f699a3a5a
GianDutra <chikegang@gmail.com>
2023-08-11 17:43:50 -0300

    uploading Storybook on github

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: <Text>Testando o elemento box</Text>,
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta
+
+export const Primary: StoryObj<BoxProps> = {}
